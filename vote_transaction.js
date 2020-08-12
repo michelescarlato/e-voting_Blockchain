ffcadb50a251c36d550486d2e0a07062712ca010
michelescarlato <michele.scarlato@gmail.com>
2020-08-13 05:18:56 +0900

    Update vote_transaction.js

--- vote_transaction.js
+++ vote_transaction.js
@@ -3,35 +3,34 @@ function sendTransatcionToBigChainDB (assetdata){
       // BigchainDB server instance or testnetwork (e.g. https://example.com/api/v1/)
       //const API_PATH = 'http://192.168.100.126:9984/api/v1/'
       const API_PATH = 'http://localhost:9984/api/v1/'
-      // Create a new keypair for Alice and Bob
-      const alice = new BigchainDB.Ed25519Keypair()
+      const DNode = new BigchainDB.Ed25519Keypair()
       let createTxId
       const DistrictNode = document.querySelector('meta[name="description"]').content;
-      
+
       const metadata = {"Token":"123456789101112131415", "District":+DistrictNode+""}
       // Construct a transaction payload
-      const txCreateAliceSimple = BigchainDB.Transaction.makeCreateTransaction(
+      const txCreateDNodeSimple = BigchainDB.Transaction.makeCreateTransaction(
               assetdata,
               metadata,
               // A transaction needs an output
               [ BigchainDB.Transaction.makeOutput(
-                              BigchainDB.Transaction.makeEd25519Condition(alice.publicKey))
+                              BigchainDB.Transaction.makeEd25519Condition(DNode.publicKey))
               ],
-              alice.publicKey
+              DNode.publicKey
       )
 
-      // Sign the transaction with private keys of Alice to fulfill it
-      const txCreateAliceSimpleSigned = BigchainDB.Transaction.signTransaction(txCreateAliceSimple, alice.privateKey)
+      // Sign the transaction with private keys of DNode to fulfill it
+      const txCreateDNodeSimpleSigned = BigchainDB.Transaction.signTransaction(txCreateDNodeSimple, DNode.privateKey)
       // Send the transaction off to BigchainDB
       let conn = new BigchainDB.Connection(API_PATH)
-      conn.postTransactionCommit(txCreateAliceSimpleSigned)
+      conn.postTransactionCommit(txCreateDNodeSimpleSigned)
       .then(res => {
             createTxId = res.id
             document.body.innerHTML ='<h3>Transaction created</h3>';
             document.body.innerHTML+=API_PATH
             document.body.innerHTML+='transactions/'
-            document.body.innerHTML+=txCreateAliceSimpleSigned.id
-            document.body.innerhtml.href = API_PATH + 'transactions/' + txCreateAliceSimpleSigned.id
+            document.body.innerHTML+=txCreateDNodeSimpleSigned.id
+            document.body.innerhtml.href = API_PATH + 'transactions/' + txCreateDNodeSimpleSigned.id
 
         })
       return;
