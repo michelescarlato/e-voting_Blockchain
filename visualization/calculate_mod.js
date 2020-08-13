7f41246968fbc2b5c096e38c0d08bb5fcb02696d
michele.scarlato <michele.scarlato@gmail.com>
2020-08-13 11:34:11 +0900

    data visualization

---
+++ visualization/calculate_mod.js
@@ -0,0 +1,46 @@
+
+function Calculate(Parameter, Indicator, querySortByCount) {
+//query arriva come parametro da graph, in questo caso sto lavorando su queryGender.
+// Bisogna capire come passarlo tramite la GET, e fargli fare poi il match
+// dentro l'endpoint, dove ovviamente queryGender e' definita.
+// DOPO Pensare poi come selezionare il tipo di survey.
+const xhttp = new XMLHttpRequest();
+const MainEndpoint = "http://localhost:3000/"
+
+//DNode parameter
+const queryMatch = document.querySelector('meta[name="description"]').content;
+console.log (queryMatch);
+
+var params = "querySortByCount="+querySortByCount+"&queryMatch="+queryMatch+"";
+
+xhttp.open("GET", "http://localhost:3000"+"?"+params, false);
+console.log("GET", "http://localhost:3000"+"?"+params)
+xhttp.send(null);
+
+const response = JSON.parse(xhttp.responseText);
+var obj = JSON.stringify(response, null, 2)
+var _id = [];
+var count = [];
+var docs = JSON.parse(obj)
+var keys = Object.keys(docs);
+for(var i=0; i<keys.length; i++){
+  var key = keys[i];
+  _id[i] = docs[key]._id
+  count[i] = docs[key].count
+}
+console.log(Parameter+":");
+console.log(_id);
+console.log("Number of "+Parameter+" repeated:");
+console.log(count);
+
+let trace = {
+  x: _id,
+  y: count,
+  marker: {color: 'rgb(55, 83, 109)'},
+  type:'bar'
+}
+let layout = {
+  title: ""+Indicator+"." ,
+  yaxis: {title: "Numbers of entries for "+Parameter},
+  xaxis: {title: Parameter} }
+  Plotly.newPlot(document.getElementById('graph'), [trace], layout);
---
+++ visualization/myFunctions_mod.js
@@ -0,0 +1,25 @@
+function myFunctionETIS() {
+  const Parameter = $('#mySelectETIS').val().split(',')[0];
+  const Indicator = $('#mySelectETIS').val().split(',')[1];
+  const querySortByCount = $('#mySelectETIS').val().split(',')[2];
+  console.log(querySortByCount)
+  Calculate(Parameter, Indicator, querySortByCount);
+}
+
+/*
+function myFunctionSDA() {
+  const Parameter = $('#mySelectSDA').val().split(',')[0];
+  const Indicator = $('#mySelectSDA').val().split(',')[1];
+  const querySortByCount = $('#mySelectSDA').val().split(',')[2];
+  console.log(querySortByCount)
+  Calculate(Parameter, Indicator, querySortByCount);
+}
+
+function myFunctionCRG() {
+  const Parameter = $('#mySelectCRG').val().split(',')[0];
+  const Indicator = $('#mySelectCRG').val().split(',')[1];
+  const querySortByCount = $('#mySelectCRG').val().split(',')[2];
+  console.log(querySortByCount)
+  Calculate(Parameter, Indicator, querySortByCount);
+}
+*/
